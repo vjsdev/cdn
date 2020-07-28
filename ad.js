8a4c3f3c4c1edb38a7b497094845da7d05374160
VIpiin J S <vipin.john@gmail.com>
2020-07-28 20:15:52 +0530

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,6 +1,6 @@
-loadAd(){
-
-document.body.style.backgroundColor = "red";
-console.log("Full window object", window);
-console.log("Parent window", window.parent);
+function loadAd()
+{
+  document.body.style.backgroundColor = "black";
+  console.log("Full window object", window);
+  console.log("Parent window", window.parent);
 }
