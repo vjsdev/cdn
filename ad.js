d105348f1f127be3bb6a095003fffceca53aaa52
VIpiin J S <vipin.john@gmail.com>
2020-07-28 20:52:50 +0530

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,6 +1,7 @@
-// To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ad.js
-
 document.addEventListener("DOMContentLoaded", function(){
+  
+  // To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ad.js
+  
   document.body.style.backgroundColor = "black";
   debugger;
   console.log("Full window object", window);
