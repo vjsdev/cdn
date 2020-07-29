ca02fde9028de36b03c0b3b33e5e144400098ed5
VIpiin J S <vipin.john@gmail.com>
2020-07-29 19:30:31 +0530

    Update and rename ba2.js to ba3.js

--- ba3.js
+++ ba3.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
   // To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ba2.js
   
   debugger;
-  document.getElementById("advertisement").innerHTML = "<a href='https://vjsdev.github.io/'><img src='https://vjsdev.github.io/img/ba.jpg' /></a>";
+  document.getElementById("advertisement").innerHTML = "<a target='_parent' href='https://vjsdev.github.io/'><img src='https://vjsdev.github.io/img/ba.jpg' /></a>";
   document.cookie = "temp=" + new Date()+ ";domain=.z19.web.core.windows.net";
   try{
     window.parent.document.cookie = "temp=" + new Date() + ";domain=.z19.web.core.windows.net";
