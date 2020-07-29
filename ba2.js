c8e028f965c7dff1a7cceeb7c02ba11a1776dd72
VIpiin J S <vipin.john@gmail.com>
2020-07-29 19:13:09 +0530

    Update and rename ba1.js to ba2.js

---
+++ ba2.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+  // To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ba2.js
+  
+  debugger;
+  document.getElementById("advertisement").innerHTML = "<a href='https://vjsdev.github.io/'><img src='https://vjsdev.github.io/img/ba.jpg' /></a>";
+  document.cookie = "temp=" + new Date()+ ";domain=.z19.web.core.windows.net";
+  try{
+    window.parent.document.cookie = "temp=" + new Date() + ";domain=.z19.web.core.windows.net";
+  }
+  catch(err){
+    console.log("Error while setting up parent window cookie",err);
+  }
+});
+
+function showWindow()
+{
+  console.log("Full window object", window);
+}
