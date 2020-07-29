8c9e8e24a3f03c4ca7bb5127115b1c238d610054
VIpiin J S <vipin.john@gmail.com>
2020-07-29 18:57:57 +0530

    Update ba.js

--- ba.js
+++ ba.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", function(){
   // To update jsdeliver cache, please use https://purge.jsdelivr.net/gh/vjsdev/cdn@master/ad.js
   
   debugger;
-  document.getElementById("advertisement").innerHTML = "<img src='https://vjsdev.github.io/img/ba.jpg' />";
+  document.getElementById("advertisement").innerHTML = "<a href='https://vjsdev.github.io/'><img src='https://vjsdev.github.io/img/ba.jpg' /></a>";
+  document.cookie = "temp=" + new Date();
+  try{
+    window.parent.document.cookie = "temp=" + new Date();
+  }
+  catch{
+  }
 });
 
 function showWindow()
